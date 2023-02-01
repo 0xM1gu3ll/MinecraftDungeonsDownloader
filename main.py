00af0f61af8d0092cb17b404bfc06e8b9b9f0fc1
Miguel Caparroz <0xm1gu3l@Miguels-MacBook-Air.local>
2023-02-01 14:17:54 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import os
+
+def get_manifest():
+    try:
+        if os.path.exists("manifest.json"):
+            print("It's not necessary to download the manifest")
+            create_folders_and_files()
+        get_manifest_url = "https://piston-meta.mojang.com/v1/products/dungeons/f4c685912beb55eb2d5c9e0713fe1195164bba27/windows-x64.json"
+        response = requests.get(get_manifest_url)
+
+        manifest_url = response.json()['dungeons'][0]['manifest']['url']
+        manifest = requests.get(str(manifest_url))
+        write_manifest = open("manifest.json", "w")
+        write_manifest.write(str(manifest.json()).replace("'", '"').replace("False", "false").replace("True", "true"))
+    except Exception:
+        print("It was not possible to download the manifest", Exception)
+def create_folders_and_files():
+    print("Now it's time to create the files and folders!")
+    manifest = open("manifest.json")
+    data = json.load(manifest)
+    for i in data["files"]:
+        if not os.path.exists("Dungeons/" + i):
+            if str(i).find(".") == -1:
+                print("Creating Folder", i)
+                os.mkdir("Dungeons/" + i)
+            else:
+                print("Downloading files")
+                open("Dungeons/" + i, "a").close()
+def download_assets():
+    print("Now it's time to download the assets and files")
+    manifest = open("manifest.json")
+    data = json.load(manifest)
+    for file_name, file_data in data['files'].items():
+        if str(file_name).find(".") == -1:
+            print("Skipping folder")
+        else:
+            for download_type, download_data in file_data['downloads'].items():
+                raw_url = file_data["downloads"]["raw"]["url"]
+                print(raw_url)
+                response = requests.get(raw_url)
+                with open("Dungeons/" + file_name, "wb") as file:
+                    file.write(response.content)
+
+get_manifest()
+create_folders_and_files()
+download_assets()
