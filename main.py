22cdb092c983e4afb728879b2ac75518296c50ff
Miguel Caparroz <0xm1gu3l@Miguels-MacBook-Air.local>
2023-02-01 14:44:14 -0400

    Try and Exception are now in the code

--- main.py
+++ main.py
@@ -14,35 +14,42 @@ def get_manifest():
         manifest = requests.get(str(manifest_url))
         write_manifest = open("manifest.json", "w")
         write_manifest.write(str(manifest.json()).replace("'", '"').replace("False", "false").replace("True", "true"))
-    except Exception:
-        print("It was not possible to download the manifest", Exception)
+    except Exception as error:
+        print("Something went wrong: ", error)
 def create_folders_and_files():
-    print("Now it's time to create the files and folders!")
-    manifest = open("manifest.json")
-    data = json.load(manifest)
-    for i in data["files"]:
-        if not os.path.exists("Dungeons/" + i):
-            if str(i).find(".") == -1:
-                print("Creating Folder", i)
-                os.mkdir("Dungeons/" + i)
-            else:
-                print("Downloading files")
-                open("Dungeons/" + i, "a").close()
+    try:
+        print("Now it's time to create the files and folders!")
+        manifest = open("manifest.json")
+        data = json.load(manifest)
+        for i in data["files"]:
+            if not os.path.exists("Dungeons/" + i):
+                if str(i).find(".") == -1:
+                    print("Creating Folder", i)
+                    os.mkdir("Dungeons/" + i)
+                else:
+                    print("Downloading files")
+                    open("Dungeons/" + i, "a").close()
+    except Exception as error:
+        print("Something went wrong: ", error)
 def download_assets():
-    print("Now it's time to download the assets and files")
-    manifest = open("manifest.json")
-    data = json.load(manifest)
-    for file_name, file_data in data['files'].items():
-        if str(file_name).find(".") == -1:
-            print("Skipping folder")
-        else:
-            for download_type, download_data in file_data['downloads'].items():
-                raw_url = file_data["downloads"]["raw"]["url"]
-                print(raw_url)
-                response = requests.get(raw_url)
-                with open("Dungeons/" + file_name, "wb") as file:
-                    file.write(response.content)
+    try:
+        print("Now it's time to download the assets and files")
+        manifest = open("manifest.json")
+        data = json.load(manifest)
+        for file_name, file_data in data['files'].items():
+            if str(file_name).find(".") == -1:
+                print("Skipping folder")
+            else:
+                for download_type, download_data in file_data['downloads'].items():
+                    raw_url = file_data["downloads"]["raw"]["url"]
+                    print(raw_url)
+                    response = requests.get(raw_url)
+                    with open("Dungeons/" + file_name, "wb") as file:
+                        file.write(response.content)
+    except Exception as error:
+        print("Something went wrong: ", error)
 
 get_manifest()
 create_folders_and_files()
 download_assets()
+print("")
